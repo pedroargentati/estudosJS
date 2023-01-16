fbfdba7488adad206ce16ec9f47912ea61233989
Pedro Argentati <pedro.argentatii@gmail.com>
2023-01-15 21:51:28 -0300

    features

--- node/src/cli.js
+++ node/src/cli.js
@@ -4,12 +4,24 @@ import pegaArquivo from "./index.js";
 
 const caminho = process.argv;
 
-function imprimeLista(resultados) {
-    console.log(chalk.yellow('Lista de Links: '), resultados);
+function imprimeLista(resultados, identificador = '') {
+    console.log(
+        chalk.yellow('Lista de Links: '),
+        chalk.black.bgGreen(identificador),
+        resultados);
 }
 
 async function processaTexto(argumentos) {
   const caminho = argumentos[2];
+
+  try {
+    fs.lstatSync(caminho);
+  } catch (error) {
+    if (error.code === 'ENOENT') {
+        console.log(chalk.red('Arquivo ou diretório não existe.'));
+        return;
+    }
+  }
 
   if (fs.lstatSync(caminho).isFile()) {
     const resultado = await pegaArquivo(caminho);
@@ -19,7 +31,7 @@ async function processaTexto(argumentos) {
 
     arquivos.forEach(async (nomeArquivo) => {
         const lista = await pegaArquivo(`${caminho}/${nomeArquivo}`);
-        imprimeLista(lista);
+        imprimeLista(lista, nomeArquivo);
     });
   }
 }
