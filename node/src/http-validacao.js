46c666b9b5d114465f08f39f9078686c6a5850b1
Pedro Argentati <pedro.argentatii@gmail.com>
2023-01-15 22:49:42 -0300

    features

--- node/src/http-validacao.js
+++ node/src/http-validacao.js
@@ -1,3 +1,5 @@
+import chalk from "chalk";
+
 function extraiLinks(arrLinks) {
   return arrLinks.map((objLink) => Object.values(objLink).join());
 }
@@ -5,18 +7,31 @@ function extraiLinks(arrLinks) {
 async function checaStatus(listaURLs) {
   const arrayStatus = await Promise.all(
     listaURLs.map(async (url) => {
-      const response = await fetch(url);
-      return response.status;
+      try {
+        const response = await fetch(url);
+        return response.status;
+      } catch (error) {
+        return manejaErros(error);
+      }
     })
   );
   return arrayStatus;
+}
+
+function manejaErros(erro) {
+  if (erro.cause.code === 'ENOTFOUND') {
+	return 'Erro: A página não foi encontrada.';
+  } else {
+	return 'Erro inesperado.'
+  }
 }
 
 export default async function listaValidada(listaLinks) {
   const links = extraiLinks(listaLinks);
   const status = await checaStatus(links);
 
-  return status;
+  return listaLinks.map((obj, idx) => ({
+    ...obj,
+    status: status[idx],
+  }));
 }
-
-// [gatinho salsicha](http://gatinhosalsicha.com.br/)
